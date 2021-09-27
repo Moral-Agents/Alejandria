0bf1dfca6ec52e48753a1cd0e8cbe920dcb3c469
GustavoBecerra1109 <gbecerra2015@gmail.com>
2021-09-27 10:01:15 -0500

    Add ICourseRepository

---
+++ Backend/AlejandriaApi/Alejandria.DataAccess/ICourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using Alejandria.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Alejandria.DataAccess
+{
+    public interface ICourseRepository
+    {
+        ICollection<User> GetCollection(string filter);
+
+        User GetItem(int id);
+
+        void Create(User entity);
+
+        void Update(User entity);
+
+        void Delete(int id);
+    }
+}
