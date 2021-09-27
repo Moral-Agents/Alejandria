bdcca206bba883ef27a2c94449b3769d7df46396
GustavoBecerraB <U201915451@upc.edu.pe>
2021-09-27 09:05:30 -0500

    Add CourseDTO

---
+++ Backend/AlejandriaApi/Alejandria.Dtos/CourseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Alejandria.Dtos
+{
+	public class CourseDto
+	{
+	public string Name { get; set; }
+	public string TeacherName { get; set; }
+	public bool TeacherLink { get; set; }
+	public int TeacherCode { get; set; }
+	public string TeacherMessage { get; set; }
+	}
+}
