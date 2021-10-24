c5035405a3fc7b26a8c7329b758a41d23fd9bf40
GustavoBecerra1109 <gbecerra2015@gmail.com>
2021-10-24 15:53:34 -0500

    Creation of Rating Repository

---
+++ Backend/AlejandriaApi/Alejandria.DataAccess/IRatingRepository.cs
@@ -0,0 +1,18 @@
+﻿using Alejandria.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alejandria.DataAccess
+{
+    public interface IRatingRepository
+    {
+        Task<ICollection<Rating>> GetCollection();
+        Task<ICollection<Rating>> GetCollectionByCourseId(int courseId);
+        Task<Rating> GetItem(int id);
+        Task Create(Rating entity);
+        Task Update(Rating entity);
+        Task Delete(int id);
+    }
+}
