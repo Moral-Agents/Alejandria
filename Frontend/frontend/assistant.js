e5870b9fa2b473d06c0434a262be1a5f5744d576
u201922331 <64971585+u201922331@users.noreply.github.com>
2021-11-21 16:09:29 -0500

    Nuevas opciones para cada Select

--- Frontend/frontend/assistant.js
+++ Frontend/frontend/assistant.js
@@ -7,21 +7,42 @@ import { Form, Button, Row, Container, Col} from 'react-bootstrap'
 import Select from 'react-select'
 
 const opt_courses = [
-    {value: 0, label: "string"},
+    {value: 0, label: "Computación Gráfica"},
     {value: 1, label: "Liberalismo Económico"},
+    {value: 2, label: "Matemática Básica"},
+    {value: 3, label: "Contabilidad"},
+    {value: 4, label: "Programación Orientada a Objetos"},
 ];
 
 const opt_universities = [
     {value: 0, label: "Universidad Peruana de Ciencias Aplicadas"},
+    {value: 1, label: "Universidad Nacional Mayor de San Marcos"},
+    {value: 2, label: "Pontificia Universidad Católica del Perú"},
+    {value: 3, label: "Universidad de Ingeniería y Tecnología"},
+    {value: 4, label: "Universidad Ricardo Palma"}
 ];
 
-const opt_rankings = [
-    {value: 0, label: "Excelente"},
-    {value: 1, label: "Bueno"},
+const opt_strict_level = [
+    {value: 0, label: "Muy estricto"},
+    {value: 1, label: "Un poco estricto"},
+    {value: 2, label: "Moderado"},
+    {value: 3, label: "Un poco relajado"},
+    {value: 4, label: "Muy relajado"},
+];
+
+const opt_qualifies = [
+    {value: 0, label: "Alto"},
+    {value: 1, label: "Medio"},
+    {value: 2, label: "Bajo"},
+];
+
+const opt_explains = [
+    {value: 0, label: "Todo claro"},
+    {value: 1, label: "Más o menos claro"},
     {value: 2, label: "Regular"},
-    {value: 3, label: "Malo"},
-    {value: 4, label: "Muy malo"},
-]
+    {value: 3, label: "Un poco confuso"},
+    {value: 4, label: "Muy Confuso"},
+];
 
 function Assistant() {
     return (
@@ -40,13 +61,13 @@ function Assistant() {
                                 <Select options={opt_courses} className="mb-3"/>
 
                                 <Form.Label>Quiero un profesor...</Form.Label>
-                                <Select options={opt_rankings} className="mb-3"/>
+                                <Select options={opt_strict_level} className="mb-3"/>
 
                                 <Form.Label>Que califique...</Form.Label>
-                                <Select options={opt_rankings} className="mb-3"/>
+                                <Select options={opt_qualifies} className="mb-3"/>
 
                                 <Form.Label>Que explique...</Form.Label>
-                                <Select options={opt_rankings} className="mb-3"/>
+                                <Select options={opt_explains} className="mb-3"/>
                                 </Col>
                             </Form.Group>
                         </div>
