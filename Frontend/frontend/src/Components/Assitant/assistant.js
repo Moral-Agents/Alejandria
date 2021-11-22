a25684cd9362a6ce3485779970cc9a53bb98bb93
u201922331 <64971585+u201922331@users.noreply.github.com>
2021-11-21 19:23:47 -0500

    Create assistant.js

---
+++ Frontend/frontend/src/Components/Assitant/assistant.js
@@ -0,0 +1,94 @@
+/*
+import { useNavigate } from 'react-router';
+*/
+import { Form, Button, Row, Container, Col} from 'react-bootstrap'
+import Select from 'react-select'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+const opt_courses = [
+    {value: 0, label: "Computación Gráfica"},
+    {value: 1, label: "Liberalismo Económico"},
+    {value: 2, label: "Matemática Básica"},
+    {value: 3, label: "Contabilidad"},
+    {value: 4, label: "Programación Orientada a Objetos"}
+];
+
+const opt_institutions = [
+    {value: 0, label: "Universidad Peruana de Ciencias Aplicadas"},
+    {value: 1, label: "Universidad Nacional Mayor de San Marcos"},
+    {value: 2, label: "Pontificia Universidad Católica del Perú"},
+    {value: 3, label: "Universidad de Ingeniería y Tecnología"},
+    {value: 4, label: "Universidad Ricardo Palma"}
+];
+
+const opt_strict_level = [
+    {value: 0, label: "Muy estricto"},
+    {value: 1, label: "Un poco estricto"},
+    {value: 2, label: "Moderado"},
+    {value: 3, label: "Un poco relajado"},
+    {value: 4, label: "Muy relajado"},
+];
+
+const opt_qualifies = [
+    {value: 0, label: "Alto"},
+    {value: 1, label: "Medio"},
+    {value: 2, label: "Bajo"},
+];
+
+const opt_explains = [
+    {value: 0, label: "Todo claro"},
+    {value: 1, label: "Más o menos claro"},
+    {value: 2, label: "Regular"},
+    {value: 3, label: "Un poco confuso"},
+    {value: 4, label: "Muy Confuso"},
+];
+
+function Assistant() {
+    /*
+    const [opts_courses, setCourses] = useState({});
+    const [opts_institutions, setInstitutions] = useState({});
+
+    const updateOptions = React.useCallback(async(e) => {
+        const request = await axios.get(`https://sleepy-reaches-77294.herokuapp.com/api/v1/Teacher`);
+        setCourses({value:request.data["id"]-1, label:request.data["course"]});
+        setInstitutions({value:request.data["id"]-1, label:request.data["institution"]});
+
+        console.log(request.data)
+    }, []);
+    */
+
+    return (
+        <div>
+            <Container className="w-25 mt-5 mb-5">
+                <h2 className="mb-4">Asistente virtual</h2>
+                <Row className="align-items-center">
+                    <Form>
+                        <div>
+                            <Form.Group className="mb-3">
+                                <Col>
+                                <Form.Label>¿De qué universidad eres?</Form.Label>
+                                <Select options={opt_institutions} className="mb-3"/>
+
+                                <Form.Label>¿Qué curso vas a llevar?</Form.Label>
+                                <Select options={opt_courses} className="mb-3"/>
+
+                                <Form.Label>Quiero un profesor...</Form.Label>
+                                <Select options={opt_strict_level} className="mb-3"/>
+
+                                <Form.Label>Que califique...</Form.Label>
+                                <Select options={opt_qualifies} className="mb-3"/>
+
+                                <Form.Label>Que explique...</Form.Label>
+                                <Select options={opt_explains} className="mb-3"/>
+                                </Col>
+                            </Form.Group>
+                        </div>
+                    </Form>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default Assistant;
